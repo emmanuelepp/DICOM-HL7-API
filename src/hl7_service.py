008a7abf9d9227071460ed0cf562a04dd847312e
emmanuelepp <emmanuelponciano@gmail.com>
2025-02-10 00:24:58 -0500

    Fix incorrect MSH segment initialization by ensuring MSH.1 is | and MSH.2 is ^~\& without duplication

--- src/hl7_service.py
+++ src/hl7_service.py
@@ -1,41 +1,44 @@
 import hl7
+from datetime import datetime
 
 
 def generate_hl7_message(dicom_metadata):
-    """
-    Generate an HL7 message based on DICOM metadata
-    """
+    """Generate a correctly formatted HL7 message from DICOM metadata."""
     msg = hl7.Message()
+    current_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # Segment MSH (Message Header)
-    msh = hl7.Segment("|")
+    # Segment MSH
+    msh = hl7.Segment()
     msh.append("MSH")
+    msh.append("|")
     msh.append("^~\\&")
     msh.append("FlaskDICOMApp")
     msh.append("Hospital")
     msh.append("ReceivingApp")
     msh.append("ReceivingFac")
-
-    # Date/Time of Message
-    msh.append(dicom_metadata.get("StudyDate", "UNKNOWN"))
+    msh.append(current_datetime)
     msh.append("")
     msh.append("ADT^A01")
     msh.append("MSG0001")
     msh.append("P")
     msh.append("2.5")
-
     msg.append(msh)
 
-    # Segment PID (Patient Identification)
-    pid = hl7.Segment("|")
+    patient_id = dicom_metadata.get("PatientID", "UNKNOWN")
+    patient_name = dicom_metadata.get(
+        "PatientName", "UNKNOWN").replace(",", "^")
+    patient_birthdate = dicom_metadata.get("PatientBirthDate", "UNKNOWN")
+
+    # Segment PID
+    pid = hl7.Segment()
     pid.append("PID")
     pid.append("1")
-    pid.append(dicom_metadata.get("PatientID", "UNKNOWN"))
     pid.append("")
+    pid.append(f"{patient_id}^^^DICOM")
     pid.append("")
-    pid.append(dicom_metadata.get("PatientName", "UNKNOWN"))
+    pid.append(patient_name)
     pid.append("")
-    pid.append(dicom_metadata.get("PatientBirthDate", "UNKNOWN"))
+    pid.append(patient_birthdate)
     pid.append("M")
 
     msg.append(pid)
